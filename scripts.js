cb8fd48e5d1fe9efad0d929bbb3b6fe9e80005b6
Karl Lowe <247839965+karllowe@users.noreply.github.com>
2026-01-15 16:55:42 +0000

    fixed error where book shelf contents were repeat

--- scripts.js
+++ scripts.js
@@ -13,45 +13,37 @@ function Book(title, author, pageNumber) {
 function addBookToLibrary(title, author, pageNumber) {
     const newBook = new Book(title, author, pageNumber);
     myLibrary.push(newBook);
+
+    const bookShelf = document.querySelector(".bookShelf");
+    const div = document.createElement("div");
+    const bookTitle = document.createElement("h3");
+    bookTitle.textContent = newBook.title;
+    bookTitle.classList.add("bookTitle")
+    
+    const bookAuthor = document.createElement("p");
+    bookAuthor.textContent = `by ${newBook.author}`;
+    bookAuthor.classList.add("author")
+    
+    const pageCount = document.createElement("p");
+    pageCount.textContent = `${newBook.pageNumber} pages`;
+    pageCount.classList.add("pageCount")
+
+    const uid = document.createElement("p");
+    uid.textContent = `UUID: ${newBook.id}`;
+    uid.classList.add("id");
+
+    div.appendChild(bookTitle);
+    div.appendChild(bookAuthor);
+    div.appendChild(pageCount);
+    div.appendChild(uid);
+
+    bookShelf.appendChild(div);
 };
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolken", 1000);
 addBookToLibrary("The Lord of the Rings", "J.R.R. Tolken", 2000);
 addBookToLibrary("The Shinning", "Stephen King", 800);
 addBookToLibrary("IT", "Stephen King", 999)
-
-function createBookDiv (array) {
-    array.forEach((item) => {
-        const bookShelf = document.querySelector(".bookShelf");
-        const div = document.createElement("div");
-        const bookTitle = document.createElement("h3");
-        bookTitle.textContent = item.title;
-        bookTitle.classList.add("bookTitle")
-        
-        const bookAuthor = document.createElement("p");
-        bookAuthor.textContent = `by ${item.author}`;
-        bookTitle.classList.add("author")
-        
-        const pageCount = document.createElement("p");
-        pageCount.textContent = `${item.pageNumber} pages`;
-        bookTitle.classList.add("pageCount")
-
-        const uid = document.createElement("p");
-        uid.textContent = `UUID: ${item.id}`;
-        uid.classList.add("id");
-
-        div.appendChild(bookTitle);
-        div.appendChild(bookAuthor);
-        div.appendChild(pageCount);
-        div.appendChild(uid);
-
-        bookShelf.appendChild(div);
-
-        }
-    )
-};
-
-createBookDiv(myLibrary);
 
 const newBookWindow = document.querySelector("dialog");
 const showButton = document.querySelector("#newBook");
@@ -75,6 +67,4 @@ submitButton.addEventListener("click", (e) => {
     const pages = formData.get("pages")
 
     addBookToLibrary(title, author, pages);
-    createBookDiv(myLibrary);
-
 });
